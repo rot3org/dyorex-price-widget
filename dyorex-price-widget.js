5388ce46ef6fdffad029e80311074a66204ef464
altug0 <0@altugoren.dev>
2023-01-02 18:56:01 +0300

    final version

--- dyorex-price-widget.js
+++ dyorex-price-widget.js
@@ -9,6 +9,7 @@ async function main() {
     widgetNode.appendChild(logoWrapper);
     widgetNode.appendChild(priceContainer);
     widget.appendChild(widgetNode)
+    
 }
 
 function widgetCreater() {
@@ -41,19 +42,32 @@ async function createPriceContainer() {
 
 async function createPriceWrapper() {
     let priceWrapper = document.createElement('div');
+    priceWrapper.setAttribute('id', 'dyorexPriceWrapper')
     priceWrapper.setAttribute('style', 'position: relative; width: 100%;height: 100%;box-sizing: content-box;display: flex;z-index: 1;')
+    
     let banner = await createBanner();
+    let endBanner = await createBanner();
+
     priceWrapper.appendChild(banner);
+    
     let cells = await addPriceCell();
+    let endCells = await addPriceCell();
+
     for (const cell of cells) {
         priceWrapper.appendChild(cell)
     }
+    priceWrapper.appendChild(endBanner)
+    for (const cell of endCells) {
+        priceWrapper.appendChild(cell)
+    }
+    let cellsWidth = (cells.length)*125;
     let wrapperAnimation = [
-        {transform : 'translate3d(-1500px, 0px, 0px)'}
+        {transform : `translate3d(-${904+cellsWidth}px, 0px, 0px)`}
     ]
-    let wrapperTiming = { duration : 40000, delay: 3000 ,iterations : Infinity }
+    let wrapperTiming = { duration : 13000, delay: 2000 ,iterations : Infinity }
     
     priceWrapper.animate(wrapperAnimation,wrapperTiming)
+    timeController(904,cellsWidth)
     return priceWrapper;
 }
 
@@ -61,17 +75,19 @@ async function createBanner() {
     let bannerWrapper = document.createElement('div');
     bannerWrapper.setAttribute('style', 'box-sizing: border-box;flex-shrink: 0;height: 100%;position: relative;width: auto;margin: 0 5px 0 0;')
     let bannerLink = document.createElement('a');
-    bannerLink.setAttribute('href', 'https://dyorex.com')
+    let options = document.getElementById('dyorexScript');
+    let utmParams = options.getAttribute('data-utm').split(',');
+    bannerLink.setAttribute('href', `https://dyorex.com/?utm_source=${utmParams[0]}&utm_medium=${utmParams[1]}&utm_campaign=${utmParams[2]}`);
     bannerLink.setAttribute('target', '_blank')
     bannerLink.setAttribute('style', 'text-decoration: none;display: flex;justify-content: center;align-items: flex-start;flex-direction: column;position: relative;height: 100%;color: #000;background: #ffff;')
     let bannerText = document.createElement('p')
-    bannerText.setAttribute('style', 'margin: 0;padding: 0px 15px;border: 0;text-align: center;font-size: 16px;font-weight: 600;color: #000;')
+    bannerText.setAttribute('style', 'margin: 0;padding: 0px 15px;border: 0;text-align: center;font-size: 20px;font-weight: 600;color: #000;')
     bannerText.textContent = 'HEMEN ÜYE OL, İLK YATIRIMINA';
 
     let text1 = document.createElement('span');
     text1.textContent = " 250 ₺'ye VARAN"
     bannerText.appendChild(text1)
-    text1.setAttribute('style', 'color: #d21d2b; font-size:20px;');
+    text1.setAttribute('style', 'color: #d21d2b; font-size:24px;');
 
     let text2 = document.createElement('span');
     text2.textContent = " ÖDÜLLERİ KAZAN"
@@ -92,8 +108,9 @@ async function addPriceCell() {
     let prices = await getTickerPrices()
     let options = document.getElementById('dyorexScript');
     let currency = options.getAttribute('data-currency');
+    let excludes = options.getAttribute('data-exclude').split(',');
     
-    let cells = Object.keys(prices).filter(key => {return key.includes(currency)}).map( (key) => {
+    let cells = Object.keys(prices).filter(key => {return key.includes(currency) && !excludes.includes(key)}).map( (key) => {
         return createCell(key, prices[key].last, prices[key].percentChange);
     })
 
@@ -101,10 +118,10 @@ async function addPriceCell() {
 }
 
 function createCell(name, price, change) {
-    let pairLogo = name.includes('USDT') ? '$' : '₺'
-    name = name.replace('USDT', '').replace('TRY', '')
+    let pairLogo = name.endsWith('USDT') ? '$' : '₺'
+    name = name !== 'USDTTRY' && 'TRYUSDT' ? name.replace('USDT', '').replace('TRY', '') : name.endsWith('USDT') ? 'TRY' : 'USDT'
     let cell = document.createElement('div');
-    cell.setAttribute('style', ' box-sizing: border-box; padding: 0; flex-shrink: 0; height: 100%; position: relative; transition-property: transform,-webkit-transform; width: auto; margin: 0 5px 0 0;')
+    cell.setAttribute('style', 'width:120px; box-sizing: border-box; padding: 0; flex-shrink: 0; height: 100%; position: relative; transition-property: transform,-webkit-transform; margin: 0 5px 0 0;')
     let cellInner = document.createElement('a'); cellInner.setAttribute('style', 'box-sizing: border-box; margin: 0; text-decoration: none; display: flex; justify-content: center; align-items: flex-start; flex-direction: column; position: relative; height: 65px; padding: 10px 15px 10px 15px; background-color: #fff; color: #000;')
     
     let nameSpan = document.createElement('span');
@@ -140,5 +157,26 @@ async function getTickerPrices() {
     });
     const result = await response.json();
     return result.data;
+}
+
+async function timeController(bannerWidth) {
+    // document.getElementById('dyorexPriceWrapper').style.animationDuration('10s')
+
+    setTimeout(()=> {
+        console.log('time')
+        console.log(document.getElementById('dyorexPriceWrapper').getAnimations())
+        document.getElementById('dyorexPriceWrapper').getAnimations()[0].playbackRate = 0.2
+        console.log(document.getElementById('dyorexPriceWrapper').getAnimations())
+
+    },5300)
+
+    setInterval(() => {
+        document.getElementById('dyorexPriceWrapper').getAnimations()[0].playbackRate = 1
+        setTimeout(() => {
+            document.getElementById('dyorexPriceWrapper').getAnimations()[0].playbackRate = 0.2
+        }, 3300);
+    }, 54000);
+
+
 }
 
